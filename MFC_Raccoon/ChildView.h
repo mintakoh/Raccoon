feaad244fb0cbabf2018233eef8428633e1fee35
mintakoh <omt1537@naver.com>
2016-05-04 14:55:55 +0900

    Revert "헤더 정리2"
    This reverts commit b9867c45a8b35d115d81af915bf65b6eb32f4959.

--- MFC_Raccoon/ChildView.h
+++ MFC_Raccoon/ChildView.h
@@ -104,14 +104,69 @@ public:
 
 	CRect rect;
 
-	///////// 맵구성이미지 //////////
+	bool _bIsDrawAll;
+	char _GameState;
+	int _iAni;
+	char _iLevel;
+	char _cMap[26][33];
+	int _EnemyCount;
+
+	Raccoon _Rac;
+	Enemy	_Ene[7];
+	Item	_Item[12];
+	jump	_StandJump[10];
+	jump	_LeftShortJump[11];
+	jump	_LeftLongJump[17];
+
+
+	//더블 버퍼링을 위해  
+	CBitmap		_cBit;
+
+	//먹은 과일 수
+	char		_iEat;
+
+	//먹은 아이템의 점수 표시 시간 
+	char		_ScoreShow;
+
+	// 남은 너구리 수
+	char		_iLive;
+
+	// 남은 시간
+	int			_iTime;
+
+	// 점수
+	int			_iScore;
+
+	
+
+
 	// 기본 맵 구성 이미지 
+	CBitmap _hMap;
 	CBitmap _hMapEle[6];
+
+	//LETS
+	CBitmap _hLets;
+	// 'SCORE'
+	CBitmap		_hScore;
+	//Gameover
+	CBitmap		_hGameOver;
+	//BONUS
+	CBitmap		_hBonus;
+	//NEXT
+	CBitmap		_hNext;
+	//ENDING
+	CBitmap		_hEnding;
+
+	// 숫자
+	CBitmap		_hDigit;
+	CBitmap		_hDigit_sm;
+
 	// 항아리
 	CBitmap		_hPot;
+
 	//과일
 	CBitmap		_hFruit[4];
-
+	
 	// 너구리 이미지 
 	CBitmap		_hLeft;
 	CBitmap		_hRight;
@@ -138,89 +193,15 @@ public:
 	//아기
 	CBitmap		_hBaby;
 
-	// 숫자
-	CBitmap		_hDigit;
-	CBitmap		_hDigit_sm;
-
-	//LETS
-	CBitmap _hLets;
-	// 'SCORE'
-	CBitmap		_hScore;
-	//Gameover
-	CBitmap		_hGameOver;
-	//BONUS
-	CBitmap		_hBonus;
-	//NEXT
-	CBitmap		_hNext;
-	//ENDING
-	CBitmap		_hEnding;
-
-	//완성된 맵이미지
-	//위의 맵 구성부품으로 맵을 만든뒤 완성된 맵을 하나의 이미지로 저장
-	CBitmap _hMap;
-
-	//더블 버퍼링을 위해  
-	CBitmap		_cBit;
-
-
-	//구조체
-	Raccoon _Rac;
-	Enemy	_Ene[7];
-	Item	_Item[12];
-
-	//점프
-	jump	_StandJump[10];
-	//짧은 점프
-	jump	_LeftShortJump[11];
-	//긴 점프
-	jump	_LeftLongJump[17];
-
-	
-	//적의 갯수
-	int _EnemyCount;
-
-	//게임 상태(4가지 상태)
-	char		_GameState;
-
-	//맵정보
-	//내부적으로 맵 처리를 하기 위해
-	char		_cMap[26][33];
-
-	//레벨
-	char		_iLevel;
-
-	// 남은 너구리 수
-	char		_iLive;
-
-	//먹은 과일 수
-	char		_iEat;
-
-	// 점프를 보여줄때 필요한 카운터
-	char _JumpFrame;
-
-	//먹은 아이템의 점수 표시 시간 
-	char		_ScoreShow;
-
-	//애니메이션을 위해
-	int _iAni;
-
-	// 남은 시간
-	int			_iTime;
-
-	// 점수
-	int			_iScore;
-
 	// 먹은 과일이나 항아리 점수 증가폭 (2배씩 증가)
 	int			_iItemScoreRate;
 
-	//기본적으로 화면을 전부 그리는 것이 아니고 처음에만 전부 그리고
-	//그후에는 변하는 부분만 다시 그린다.
-	//문제는 원도우을 이동하거나 다른 원도우로 가린다면 화면을 전부 다시 그려야 한다.
-	//이를 위해서 아래의 변수가 필요하다. 
-	bool _bIsDrawAll;
-
+	// 점프를 보여줄때 필요한 카운터
+	char _JumpFrame;
 	// 너구리가 죽어서 떨어질때 음악 연주 상태
 	BOOL _bIsDrop_Sound;
 
+
+	
 };
 
